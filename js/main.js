da0a7d2dc05b2d06f4e7d5b8d10a70adec378515
TownOfGoog <95412264+TownOfGoog@users.noreply.github.com>
2023-06-07 14:56:51 +0200

    Camera can now rotate around the soon to be ball

--- js/main.js
+++ js/main.js
@@ -1,15 +1,74 @@
 const three = new Threestrap.Bootstrap();
 
-const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff3333 }));
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff3333 })
+);
 
 three.scene.add(cube);
 
 three.camera.position.set(1, 1, 2);
 three.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
+let isDragging = false;
+let previousMousePosition = {
+  x: 0,
+  y: 0
+};
+
+
+
+// Add mouse event listeners
+document.addEventListener('mousedown', onMouseDown);
+document.addEventListener('mousemove', onMouseMove);
+document.addEventListener('mouseup', onMouseUp);
+
+function onMouseDown(event) {
+  if (event.button === 0) {
+    isDragging = true;
+    previousMousePosition = {
+      x: event.clientX,
+      y: event.clientY
+    };
+  }
+}
+
+function onMouseMove(event) {
+  if (isDragging) {
+    const deltaMove = {
+      x: event.clientX - previousMousePosition.x,
+      y: event.clientY - previousMousePosition.y
+    };
+
+    const deltaRotationQuaternion = new THREE.Quaternion()
+      .setFromEuler(new THREE.Euler(
+        toRadians(deltaMove.y * 1),
+        toRadians(deltaMove.x * 1),
+        0,
+        'XYZ'
+      ));
+
+    cube.quaternion.multiplyQuaternions(deltaRotationQuaternion, cube.quaternion);
+    
+    previousMousePosition = {
+      x: event.clientX,
+      y: event.clientY
+    };
+  }
+}
+
+function onMouseUp(event) {
+  if (event.button === 0) {
+    isDragging = false;
+  }
+}
+
+function toRadians(angle) {
+  return angle * (Math.PI / 180);
+}
+
 three.on('update', function () {
   cube.rotateY(0.02);
-  // var t = three.Time.now;
-  // three.camera.position.set(Math.cos(t), 1.5, Math.sin(t));
-  // three.camera.lookAt(new THREE.Vector3());
 });
+
+
